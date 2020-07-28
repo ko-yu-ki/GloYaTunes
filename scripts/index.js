8c3a8a8ec52e7ef7456f8e7ded9ad7c6e576b89e
ko-yu-ki <55734445+ko-yu-ki@users.noreply.github.com>
2020-07-28 17:13:32 +0300

    первый день

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+import { radioPlayerInit } from './radioPlayer.js'; //ипортируем функцию из radioPlayer.js
+import { musicPlayerInit } from './musicPlayer.js'; //ипортируем функцию из radioPlayer.js
+import { videoPlayerInit } from './videoPlayer.js'; //ипортируем функцию из radioPlayer.js
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivationPlayer = () => {
+  temp.style.display = 'none';
+  playerBtn.forEach(item => item.classList.remove('active')); 
+  playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+playerBtn.forEach((btn, btnIndex) => {
+  btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlock[btnIndex].classList.add('active');
+  });
+});
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
